ace2a24e47381fe20deadc0ad1dab85087ec06a5
Paul Mallory <pemallory@gmail.com>
2014-10-02 16:39:23 -0500

    GradPlanner Class Start
    Started GradPlanner class for TXP1 Project

---
+++ GradPlanner/GradPlanner.java
@@ -0,0 +1,15 @@
+/**
+    This program accepts a number of competency units (CUs) per course remaining
+    in a student's degree program, and the number of CUs the student plans to
+    complete per term and outputs the number of terms left to complete based on
+    input data, the tuition cost based on the number of remaining terms, and the
+    number of months to completion, based on input data.
+*/
+public class GradPlanner
+{
+    import java.io.Scanner;
+    public static void main(String args[])
+    {
+    
+    }
+}
