5ff48027beb67afa8cee8204dcc9e1845889ad91
Paul Mallory <pemallory@gmail.com>
2014-10-03 13:04:34 -0500

    Revert "Program Skeleton Started"
    This reverts commit 1bf924f56e1c65387b4639261609c656933f650f.

--- GradPlanner/GradPlanner.java
+++ GradPlanner/GradPlanner.java
@@ -5,60 +5,11 @@
     input data, the tuition cost based on the number of remaining terms, and the
     number of months to completion, based on input data.
 */
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.logging.Logger;
-import java.util.logging.Level;
-
 public class GradPlanner
 {
+    import java.io.Scanner;
     public static void main(String args[])
     {
-        Logger.getGlobal().setLevel(Level.INFO);
-        Scanner in = new Scanner(System.in);
-        boolean programRun = true;
-        while(programRun)
-        {
-            String input = "";
-            ArrayList<Integer> compUnitTotal = new ArrayList<Integer>();
-            System.out.print("Enter courses remaining (r), units to take per term (u), or calculate (c); q to quit: ");
-            if(in.hasNext())
-            {
-                input = in.next();
-            }
-            if(input.toLowerCase().equals("r")) //user selects to enter courses
-            {
-                boolean done = false;
-                while(!done)
-                {
-                    int courseInput = 0;
-                    System.out.print("Enter remaining course units individually (-1 to return to main options): ");
-                    if(in.hasNextInt())
-                    {
-                        courseInput = in.nextInt();
-                    }
-                    if(courseInput == -1) // return to main options
-                    {
-                        done = true;
-                    }
-                    else if(courseInput > 0) // add CUs to ArrayList
-                    {
-                        compUnitTotal.add(courseInput);
-                    }
-                    else // invalid input
-                    {
-                        System.out.println("Error: please enter integers greater than 0 (or -1 to return to main options).");
-                    }
-                }
-            }
-            else if(input.toLowerCase().equals("q")) // user selects to quit program
-            {
-                programRun = false;
-            }
-            else // invalid input
-            {
-                System.out.println("Error: invalid option.");
-            }
-        }
+    
     }
 }
