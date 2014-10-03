0c252ee56abc4a8725ae56d270916385ed6e501d
Paul Mallory <pemallory@gmail.com>
2014-10-03 16:12:32 -0500

    Units per term added
    Competency units sum moved to method.  Units per term prompt added.

--- GradPlanner/GradPlanner.java
+++ GradPlanner/GradPlanner.java
@@ -17,10 +17,14 @@ public class GradPlanner
         Logger.getGlobal().setLevel(Level.INFO);
         Scanner in = new Scanner(System.in);
         boolean programRun = true;
+        ArrayList<Integer> compUnitTotal = new ArrayList<Integer>();
+        final int MONTHS_PER_TERM = 6;
+        final int COST_PER_TERM = 2890;
+        final int CUS_REQUIRED_PER_TERM = 12;
+        int totalCost = 0;
         while(programRun)
         {
             String input = "";
-            ArrayList<Integer> compUnitTotal = new ArrayList<Integer>();
             System.out.print("Enter courses remaining (r), units to take per term (u), or calculate (c); q to quit: ");
             if(in.hasNext())
             {
@@ -31,13 +35,12 @@ public class GradPlanner
                 boolean done = false;
                 while(!done)
                 {
-                    String courseInput = "";
                     System.out.print("Enter remaining course units individually (-1 to return to main options): ");
-                    courseInput = in.next();
-                    String err = "Error: please enter integers greater than 0 (or -1 to return to main options).";
+                    String courseInput = in.next();
+                    String err = "Error: please enter integers greater than zero (or -1 to return to main options).";
                     try
                     {
-                        int courseInt = Integer.parseInt(courseInput);
+                        int courseInt = Integer.parseInt(courseInput); // parseInt to avoid problems with unexpected input
                         if(courseInt == -1) // return to main options
                         {
                             done = true;
@@ -57,6 +60,49 @@ public class GradPlanner
                     }
                 }
             }
+            else if(input.toLowerCase().equals("u")) // user selects to enter units to take per term
+            {
+                boolean done = false;
+                while(!done)
+                {
+                    System.out.print("Enter the number of CUs you plan to complete per term: ");
+                    if(compUnitTotal.size() > 0)
+                    {
+                        String unitInput = in.next();
+                        try
+                        {
+                            int unitInt = Integer.parseInt(unitInput); // parseInt to avoid problems with unexpected input
+                            int compUnitsNeeded = unitSum(compUnitTotal);
+                            if(unitInt <= 0)
+                            {
+                                System.out.println("Error: please enter an integer greater than zero.");
+                            }
+                            else if(compUnitsNeeded >= CUS_REQUIRED_PER_TERM && unitInt < CUS_REQUIRED_PER_TERM)
+                            {
+                                System.out.println("Invalid: undergraduate students must take a minimum 12 CUs per term.");
+                            }
+                            else
+                            {
+                                System.out.println("OK");
+                                done = true;
+                            }
+                        }
+                        catch(NumberFormatException e)
+                        {
+                            System.out.println("Error: please enter an integer greater than zero.");
+                        }
+                    }
+                    else
+                    {
+                        System.out.println("Error: you must enter competency units to complete before entering units per term.");
+                        done = true;
+                    }
+                }
+            }
+            else if(input.toLowerCase().equals("c")) // user selects to calculate
+            {
+                
+            }
             else if(input.toLowerCase().equals("q")) // user selects to quit program
             {
                 programRun = false;
@@ -66,5 +112,14 @@ public class GradPlanner
                 System.out.println("Error: invalid option.");
             }
         }
+    }
+    public static int unitSum(ArrayList<Integer> arrayList)
+    {
+        int result = 0;
+        for(int i = 0; i < arrayList.size(); i++)
+        {
+            result = result + arrayList.get(i);
+        }
+        return result;
     }
 }
